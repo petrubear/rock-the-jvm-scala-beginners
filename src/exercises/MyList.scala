bd6bbb0a6e9c63a90521cf5e5a2a36874b0d48c1
petrubear <edison.martinez@gmail.com>
2020-05-28 19:39:24 -0500

    oop basics 4

--- src/exercises/MyList.scala
+++ src/exercises/MyList.scala
@@ -1,14 +1,14 @@
 package exercises
 
-abstract class MyList {
+abstract class MyList[+A] {
 
-  def head: Int
+  def head: A
 
-  def tail: MyList
+  def tail: MyList[A]
 
   def isEmpty: Boolean
 
-  def add(n: Int): MyList
+  def add[B >: A](n: B): MyList[B]
 
   def printElements: String
 
@@ -16,34 +16,34 @@ abstract class MyList {
 
 }
 
-object Empty extends MyList {
-  override def head: Int = throw new NoSuchElementException
+object Empty extends MyList[Nothing] {
+  override def head: Nothing = throw new NoSuchElementException
 
-  override def tail: MyList = throw new NoSuchElementException
+  override def tail: MyList[Nothing] = throw new NoSuchElementException
 
   override def isEmpty: Boolean = true
 
-  override def add(n: Int): MyList = new Cons(n, Empty).add(n)
+  override def add[B >: Nothing](n: B): MyList[B] = new Cons[B](n, Empty)
 
   override def printElements: String = ""
 }
 
-class Cons(h: Int, t: MyList) extends MyList {
-  override def head: Int = h
+class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
+  override def head: A = h
 
-  override def tail: MyList = t
+  override def tail: MyList[A] = t
 
   override def isEmpty: Boolean = false
 
-  override def add(n: Int): MyList = new Cons(n, this)
+  override def add[B >: A](n: B): MyList[B] = new Cons(n, this)
 
   override def printElements: String = if (t.isEmpty) s"$h" else s"$h ${t.printElements}"
 }
 
 object ListTest extends App {
-  var list = new Cons(1, new Cons(2, new Cons(3, Empty)))
-  println(list.tail.head)
-  println(list.add(4).head)
-  println(list.toString)
+  var listOfInts = new Cons[Int](1, new Cons[Int](2, new Cons[Int](3, Empty)))
+  var listOfStrings = new Cons[String]("a", new Cons[String]("b", new Cons[String]("c", Empty)))
+  println(listOfInts.toString)
+  println(listOfStrings.toString)
 }
 
